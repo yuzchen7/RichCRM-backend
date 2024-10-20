adf0f4c4b34d013a92fa24bb63efc40a75a9a0bc
yuzchen7 <yuzchen7@gmail.com>
2024-10-20 02:20:39 -0400

    doc: added command for class

--- richcrm-backend/utils/JwToken.js
+++ richcrm-backend/utils/JwToken.js
@@ -1,14 +1,40 @@
-import jwt from 'jsonwebtoken';
+// const jwt = require('jsonwebtoken');
+import { sign, verify, decode } from 'jsonwebtoken';
 
+/**
+ * Json Web Token Util Class 
+ * 
+ *  do not construct object with init constructor, it is utility class
+ * 
+ * @class
+ */
 class JwTokenUtil {
-    static generateAccessToken(data, secret, timeTravel = process.env.REFRESH_TOKEN_TIME_EXPIRATION) {
-        return jwt.sign(data, secret, { expiresIn: timeTravel });
+    /**
+     * generate a new Access Token
+     * 
+     * @param {object} data - data to be encoded in the token
+     * @param {string} secret - secret key to be used for signing 
+     * @param {string} [timeTravel='1h'] - time to expire the token, default is 1 hour
+     * 
+     * @return {string} encoded string with signature, format will be [encoded.encoded.signature]
+     */
+    static generateAccessToken(data, secret, timeTravel = '1h') {
+        return sign(data, secret, { expiresIn: timeTravel });
     }
 
+    /**
+     *  check if given token is valid
+     * 
+     * @param {string} token - an refresh / access token string
+     * @param {string} secret - secret key to be used for signing 
+     * @returns {boolean} true if token is valid, false otherwise
+     * 
+     * token without signature or expired will be considered invalid
+     */
     static async isValid(token, secret) {
         try {
             const promise = new Promise((resolve, reject) => {
-                jwt.verify(token, secret, (err, data) => {
+                verify(token, secret, (err, data) => {
                     if (err) {
                         let err = new Error();
                         err.message = 'error invalid token';
@@ -20,7 +46,7 @@ class JwTokenUtil {
                         }
                         resolve(retData);
                     }
-                })
+                });
             }); 
             return promise;
         } catch (err) {
@@ -28,9 +54,15 @@ class JwTokenUtil {
         }
     }
 
+    /**
+     * decode token and return its payload data
+     * 
+     * @param {string} token - an refresh / access token string
+     * @returns {object} token payload data
+     */
     static decodeToken(token) {
         try {   
-            const decoded = jwt.decode(token);
+            const decoded = decode(token);
             if (decoded !== undefined) {
                 const ret = {
                     ...decoded
@@ -46,4 +78,4 @@ class JwTokenUtil {
     }
 };
 
-export default JwTokenUtil;
+module.exports = JwTokenUtil;
--- richcrm-backend/utils/Password.js
+++ richcrm-backend/utils/Password.js
@@ -1,18 +1,57 @@
-import crypto from 'crypto';
+// const crypto = require('crypto');
+const { randomBytes, createHash } = require('crypto');
 
+/**
+ * Password Util Class 
+ * 
+ *  do not construct object with init constructor, it is utility class
+ * 
+ * @class
+ */
 class PasswordUtil {
+    /**
+     * Generates a random salt value for password hashing.
+     * 
+     * The salt is a 16-byte random value, returned as a hexadecimal string.
+     * Salts are used to ensure that hashed passwords are unique, even for
+     * identical passwords, enhancing security against rainbow table attacks.
+     * 
+     * @returns {string} A hexadecimal string representing the random salt.
+     */
     static generateSalt() {
-        return crypto.randomBytes(16).toString('hex');
+        return randomBytes(16).toString('hex');
     }
 
+    /**
+     * Encrypts the given password using a specified salt value.
+     * This method applies the RSA-SHA256 hashing algorithm to create a secure hash
+     * of the password combined with the salt. The resulting hash is returned 
+     * as a hexadecimal string, which can be stored securely.
+     *
+     * @param {string} password - The password to be encrypted.
+     * @param {string} salt - The salt value used in the encryption process.
+     * @returns {string} A hexadecimal string representing the encrypted password.
+     */
     static encrypt(password, salt) {
-        return crypto.createHash('RSA-SHA256').update(password).update(salt).digest('hex');
+        return createHash('RSA-SHA256').update(password).update(salt).digest('hex');
     }
 
+    /**
+     * Validates a entered password by comparing it with the target password.
+     * This method hashes the entered password using the provided salt and 
+     * compares the resulting hash with the target password's hash.
+     * It returns true if the hashes match, indicating the entered password 
+     * is valid; otherwise, it returns false.
+     *
+     * @param {string} targetPassword - The hashed password to compare against.
+     * @param {string} enterPassword - The password entered by the user to validate.
+     * @param {string} salt - The salt value used for hashing the entered password.
+     * @returns {boolean} True if the entered password is valid, false otherwise.
+     */
     static isValidPassword(targetPassword, enterPassword, salt) {
-        const hashedPassword = PasswordUtils.hashPassword(enterPassword, salt);
+        const hashedPassword = PasswordUtil.hashPassword(enterPassword, salt);
         return hashedPassword === targetPassword;
     }
 };
 
-export default PasswordUtil;
+module.exports = PasswordUtil;
